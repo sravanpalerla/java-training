e478bb45dbfbbc415a0dcdac545a294fa548b64b
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 09:59:48 +0530

    product quantity

---
+++ src/main/java/org/relationaloperators/ProductQuantity.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class ProductQuantity {
+    public static void main(String[] args) {
+        int product1=500;
+        int product2=470;
+        boolean b=product1>product2;
+        System.out.println(b);
+    }
+}
