9c9f850aed2980a742ef7a2535b8b57a98871011
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:12:18 +0530

    Bank balance

---
+++ src/main/java/org/relationaloperators/BankBalance.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class BankBalance {
+    public static void main(String[] args) {
+        int b1=24000;
+        int b2=25000;
+        boolean b=b1==b2;
+        System.out.println(b);
+    }
+}
