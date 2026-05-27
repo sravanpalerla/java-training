43ca17609ec4f174435b4537c28a79e9ed76ffa7
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 09:19:23 +0530

    student salary

---
+++ src/main/java/org/relationaloperators/StudentSalary.java
@@ -0,0 +1,11 @@
+package org.relationaloperators;
+
+public class StudentSalary {
+    public static void main(String[] args) {
+        int student=13000;
+        int sal=25000;
+        boolean b=student>=25000;
+        System.out.println(b);
+        
+    }
+}
