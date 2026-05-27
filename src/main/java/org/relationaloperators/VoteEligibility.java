af449595ff838c4d98851772e92c3afb153f35ea
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:28:35 +0530

    vote eligibility

---
+++ src/main/java/org/relationaloperators/VoteEligibility.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class VoteEligibility {
+    public static void main(String[] args) {
+        int age=19;
+        int eligiility=21;
+        boolean b=age>eligiility;
+        System.out.println(b);
+    }
+}
