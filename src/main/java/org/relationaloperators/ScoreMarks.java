fae4549732511de3bdd332b4ee13189e492958bf
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 09:37:48 +0530

    score marks

---
+++ src/main/java/org/relationaloperators/ScoreMarks.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class ScoreMarks {
+    public static void main(String[] args) {
+        int a=87;
+        int b=97;;
+        boolean res=a<=b;
+        System.out.println(res);
+    }
+}
