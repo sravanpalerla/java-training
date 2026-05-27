476a36b38b305f00df31cf9121adc624c83f32f4
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:25:09 +0530

    Student Attendces

---
+++ src/main/java/org/relationaloperators/SudentAttendces.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class SudentAttendces {
+    public static void main(String[] args) {
+        int a=95;
+        int b=95;
+        boolean res=a==b;
+        System.out.println(res);
+    }
+}
