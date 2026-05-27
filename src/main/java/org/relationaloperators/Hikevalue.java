83a512c4f45c254cc60d42693c4b4e0031ef27bf
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:20:46 +0530

    hike value

---
+++ src/main/java/org/relationaloperators/Hikevalue.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class Hikevalue {
+    public static void main(String[] args) {
+        int a=130;
+        int b1=120;
+        boolean b=(a+10)>b1;
+        System.out.println(b);
+    }
+}
