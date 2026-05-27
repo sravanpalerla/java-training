c4c8b437225169293d9570f513615493259f09d7
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 09:49:42 +0530

    length

---
+++ src/main/java/org/relationaloperators/Length.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class Length {
+    public static void main(String[] args) {
+        int l1=45;
+        int l2=45;
+        boolean b=l1==l2;
+        System.out.println(b);
+    }
+}
