51ae5b9b6ab49287eca15048e897ed50109f8baf
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 18:46:04 +0530

    Student exam

---
+++ src/main/java/org/logicaloperators/StudentExam.java
@@ -0,0 +1,9 @@
+package org.logicaloperators;
+
+public class StudentExam {
+    public static void main(String[] args) {
+        int marks=65;
+        int attendence=35;
+        System.out.println(marks>=35||attendence>=75);
+    }
+}
