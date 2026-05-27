49db9f6a8315930b218a79c7b326e31d4718297f
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:15:50 +0530

    bike limit speed

---
+++ src/main/java/org/relationaloperators/BikeLimitSpeed.java
@@ -0,0 +1,10 @@
+package org.relationaloperators;
+
+public class BikeLimitSpeed {
+    public static void main(String[] args) {
+        int bikespeed=60;
+        int limit=100;
+        boolean b=bikespeed<limit;
+        System.out.println(b);
+    }
+}
