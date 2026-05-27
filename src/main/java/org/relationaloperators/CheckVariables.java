3fc9eb45b5bcf9039e4ce539eb46a6c9ffccba11
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 10:07:22 +0530

    checkVariables

---
+++ src/main/java/org/relationaloperators/CheckVariables.java
@@ -0,0 +1,9 @@
+package org.relationaloperators;
+
+public class CheckVariables {
+    public static void main(String[] args) {
+        int x=230;
+        int y=340;
+        System.out.println(x<y);
+    }
+}
