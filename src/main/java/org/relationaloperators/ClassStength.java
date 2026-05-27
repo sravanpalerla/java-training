7a825e783bd9418783f58f7833febd1a75ca39e2
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-27 09:53:30 +0530

    class stength

---
+++ src/main/java/org/relationaloperators/ClassStength.java
@@ -0,0 +1,9 @@
+package org.relationaloperators;
+
+public class ClassStength {
+    public static void main(String[] args) {
+        int boys=45;
+        int girls=35;
+        System.out.println(boys>girls);
+    }
+}
