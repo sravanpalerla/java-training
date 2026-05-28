5acf38783c42278ee445118ef039d6b38e765f66
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 09:00:45 +0530

    bank loan

---
+++ src/main/java/org/logicaloperators/BankLoan.java
@@ -0,0 +1,7 @@
+public class BankLoan {
+    public static void main(String[] args) {
+        int salary=39000;
+        int credit=751;
+        System.out.println(salary>=40000||credit>=750);
+    }
+}
