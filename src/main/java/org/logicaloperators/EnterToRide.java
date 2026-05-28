20caabd559a5ae7392241b313e1bd82bdf380bd9
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 09:33:15 +0530

    enter to ride

---
+++ src/main/java/org/logicaloperators/EnterToRide.java
@@ -0,0 +1,10 @@
+package org.logicaloperators;
+
+public class EnterToRide {
+    public static void main(String[] args) {
+        int age=19;
+        float height=5.5f;
+        boolean b=(age>=19&&height>=5.4);
+        System.out.println(b);
+    }
+}
