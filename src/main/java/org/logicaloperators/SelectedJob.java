1082b89fec5aa881342558fbfd058a89a4bff21b
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 09:09:49 +0530

    selectedJob

---
+++ src/main/java/org/logicaloperators/SelectedJob.java
@@ -0,0 +1,9 @@
+package org.logicaloperators;
+
+public class SelectedJob {
+    public static void main(String[] args) {
+        int aptitudemarks=75;
+        int communicationscore=89;
+        System.out.println(aptitudemarks>=70&&communicationscore>=80);
+    }
+}
