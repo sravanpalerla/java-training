d90792eaf8b634986ca1191c331d21bf4ceb9b9b
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 18:43:17 +0530

    SmallestNumber

---
+++ src/main/java/org/conditionaloperators/SmallestNumber.java
@@ -0,0 +1,10 @@
+package org.conditionaloperators;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int a=9;
+        int b=12;
+        String s=(a>=b?"biggest":"smallest");
+        System.out.println(s);
+    }
+}
