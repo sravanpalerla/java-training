602ad4c1b75bbd5e49c6be1b4b11fe1420842b53
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 18:21:30 +0530

    Even odd

---
+++ src/main/java/org/conditionaloperators/EvenOdd.java
@@ -0,0 +1,9 @@
+package org.conditionaloperators;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int a=20;
+        String s=(a%2==0?"even":"odd");
+        System.out.println(s);
+    }
+}
