3127c33ebaf146e4c3804003b7c74ddfb943d69d
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 18:39:05 +0530

    upper method

---
+++ src/main/java/org/conditionaloperators/UpperMethod.java
@@ -0,0 +1,9 @@
+package org.conditionaloperators;
+
+public class UpperMethod {
+    public static void main(String[] args) {
+        char c='A';
+        String s=(c==c?"uppercase":"lowecase");
+        System.out.println(s);
+    }
+}
