e3f4319bbfd0807219e192af2dcd0c65bb82b6ed
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 09:24:13 +0530

    Social media

---
+++ src/main/java/org/logicaloperators/SocialMedia.java
@@ -0,0 +1,10 @@
+package org.logicaloperators;
+
+public class SocialMedia {
+    public static void main(String[] args) {
+        int likes=12000;
+        int hours=24;
+        System.out.println(likes>=10000||hours>=35);
+
+    }
+}
