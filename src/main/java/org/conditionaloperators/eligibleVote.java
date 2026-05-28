f76d89d3d721df1f626ba16b3ec1d80dff75c58f
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 18:46:23 +0530

    eligiable vote

---
+++ src/main/java/org/conditionaloperators/eligibleVote.java
@@ -0,0 +1,9 @@
+package org.conditionaloperators;
+
+public class eligibleVote {
+    public static void main(String[] args) {
+        int age=21;
+        String s=(age>18?"eligiable":"not eligiable");
+        System.out.println(s);
+    }
+}
