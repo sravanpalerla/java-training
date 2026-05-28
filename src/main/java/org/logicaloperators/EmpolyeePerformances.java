fc7b6a45f19a401975d261abbda571c4820c6fa2
sravanpalerla <sravankumapalerla@gmail.com>
2026-05-28 09:06:25 +0530

    empolyeeperformances

---
+++ src/main/java/org/logicaloperators/EmpolyeePerformances.java
@@ -0,0 +1,9 @@
+package org.logicaloperators;
+
+public class EmpolyeePerformances {
+    public static void main(String[] args) {
+        int rating=5;
+        int experience=5;
+        System.out.println(rating>=4&&experience>=4);
+    }
+}
