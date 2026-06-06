59233fc711ace72fa6b2e155a9a20abd09a38ad9
sravanpalerla <sravankumapalerla@gmail.com>
2026-06-06 10:11:00 +0530

    grade  marks

---
+++ src/main/java/org/decisionmaking/GradeMarks.java
@@ -0,0 +1,20 @@
+package org.decisionmaking;
+import java.util.Scanner;
+public class GradeMarks {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter number");
+        int a = s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        if (a > 90) {
+            System.out.println("A grade");
+        } else if (b > 75) {
+            System.out.println("b grade");
+        } else if (c > 65) {
+            System.out.println("c grade");
+        } else {
+            System.out.println("fail");
+        }
+    }
+}
